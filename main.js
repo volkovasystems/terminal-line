bbe11914b96a8ff69210de4abb41e6fd0abbaee9
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-08-12 01:43:14 +0800

    Added files from plnkr.

--- main.js
+++
@@ -1,7 +0,0 @@
-var consoleDebug = console.debug;
-console.debug = terminalLine( "console-debug", consoleDebug ); 
-
-console.debug( "Console.debug Test", "world domination", "progressively" );
-console.debug( "Console.debug Test", "world domination", "progressively" );
-
-require(  )
